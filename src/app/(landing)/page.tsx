e96738c9d4f36aebc5e22082b71950dda830e419
AUM <omjha50@gmail.com>
2025-08-02 02:53:30 +0530

    build discord ui

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -2,6 +2,7 @@ import { Heading } from "../../components/heading";
 import { MaxWidthWrapper } from "../../components/max-width-wrapper";
 import { Check } from "lucide-react"
 import { ShinyButton } from "../../components/shinny-button";
+import { MockDiscordUI } from "@/components/mock-discord-ui";
 
 const Page = () => {
   return (
@@ -40,7 +41,14 @@ const Page = () => {
           </div>
         </MaxWidthWrapper>
       </section>
-      <section></section>
+      <section className="relative bg-brand-25 pb-4">
+        <div className="absolute inset-x-0 bottom-24 top-24 bg-brand-700" />
+        <MaxWidthWrapper className="relative">
+          <div className="-m-2 rounded-xl bg-gray-900/5 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+              <MockDiscordUI />
+          </div>
+        </MaxWidthWrapper>
+      </section>
       <section></section>
       <section></section>
     </>
