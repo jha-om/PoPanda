a4ed94d98e3c77dcde90dc8a1edd7ca0cf78e5ab
AUM <omjha50@gmail.com>
2025-08-09 02:27:23 +0530

    fixed the route and created discord client

---
+++ src/app/api/v1/events/route.ts
@@ -0,0 +1,175 @@
+import { FREE_QUOTA, PRO_QUOTA } from "@/config";
+import { db } from "@/db";
+import { DiscordClient } from "@/lib/discord-client";
+import { CATEGORY_NAME_VALIDATOR } from "@/lib/validator/category-validator";
+import { NextRequest, NextResponse } from "next/server";
+import z from "zod";
+
+const REQUEST_VALIDATOR = z
+    .object({
+        category: CATEGORY_NAME_VALIDATOR,
+        fields: z.record(z.string().or(z.number()).or(z.boolean())).optional(),
+        description: z.string().optional(),
+    })
+    .strict();
+
+export const POST = async (req: NextRequest) => {
+    const authHeader = req.headers.get("Authorization");
+    console.log("authHeader::", authHeader);
+    
+    if (!authHeader) {
+        return NextResponse.json({
+            message: "Unauthorized",
+        }, { status: 401 })
+    }
+
+    if (!authHeader.startsWith("Bearer ")) {
+        return NextResponse.json({
+            message: "Invalid auth header format. Expected: 'Bearer [API_KEY]'",
+        }, { status: 401 })
+    }
+
+    const apiKey = authHeader.split(' ')[1];
+    if (!apiKey || apiKey.trim() === "") {
+        return NextResponse.json({
+            message: "Invalid API key"
+        }, { status: 401 });
+    }
+    
+    const user = await db.user.findUnique({
+        where: { apiKey },
+        include: { EventCategories: true },
+    })
+    console.log("user::", user);
+
+    if (!user) {
+        return NextResponse.json({
+            message: "Invalid API key"
+        }, { status: 401 });
+    }
+    
+    if (!user.discordId) {
+        return NextResponse.json({
+            message: "Please enter your Discord ID in your account settings"
+        }, { status: 403 });    
+    }
+
+    // *how quota system works in SaaS, is implemented below
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth() + 1;
+    const currentYear = currentDate.getFullYear();
+
+    const quota = await db.quota.findUnique({
+        where: {
+            userId: user.id,
+            month: currentMonth,
+            year: currentYear,
+        },
+    });
+
+    // why quotaLimit should never be hard-coded? 
+    // because it's very difficult to maintain, what if these hard coded are also present somewhere else, 
+    // might be very difficult to debug if issue comes;
+    // therefore we should always create a config.ts/js file into root folder;
+    const quotaLimit = user.plan === "FREE" ? FREE_QUOTA.maxEventsPerMonth : PRO_QUOTA.maxEventsPerMonth
+
+    if (quota && quota.count >= quotaLimit) {
+        return NextResponse.json({
+            message: "Monthly quota reached. Please upgrade your plan for more events.",
+        }, { status: 429 })
+    }
+
+    const discord = new DiscordClient(process.env.DISCORD_BOT_TOKEN);
+
+
+    const dmChannel = await discord.createDM(user.discordId);
+
+    let requestData: unknown;
+
+    try {
+        requestData = await req.json();
+    } catch (error) {
+        return NextResponse.json({
+            message: "Invalid JSON request body",
+        }, { status: 400 })
+    }
+
+    const validationResult = REQUEST_VALIDATOR.parse(requestData);
+
+    const category = user.EventCategories.find((c) => c.name === validationResult.category);
+
+    if (!category) {
+        return NextResponse.json({
+            message: `You don't have a category name ${validationResult.category}`
+        }, { status: 404 })
+    }
+
+    const eventData = {
+        title: `${category.emoji || '🔔'} ${category.name.charAt(0).toUpperCase() + category.name.slice(1)}`,
+        description: validationResult.description || `A new ${category.name} event has occured!`,
+        color: category.color,
+        timestamp: new Date().toISOString(),
+        fields: Object.entries(validationResult.fields || {}).map(([key, value]) => {
+            return {
+                name: key,
+                value: String(value),
+                inline: true,
+            }
+        })
+    }
+
+    const event = await db.event.create({
+        data: {
+            name: category.name,
+            formattedMessage: `${eventData.title}\n\n${eventData.description}`,
+            userId: user.id,
+            fields: validationResult.fields || {},
+            eventCategoryId: category.id
+        },
+    })
+
+    try {
+        await discord.sentEmbed(dmChannel.id, eventData);
+
+        await db.event.update({
+            where: { id: event.id },
+            data: { delieveryStatus: "DELIVERED" }
+        })
+        
+        await db.quota.upsert({
+            where: {
+                userId: user.id,
+                month: currentMonth,
+                year: currentYear,
+            },
+            update: {
+                count: {
+                    increment: 1,
+                },
+            },
+            create: {
+                userId: user.id,
+                month: currentMonth,
+                year: currentYear,
+                count: 1,
+            }
+        })
+    } catch (error) {
+        await db.event.update({
+            where: { id: event.id },
+            data: { delieveryStatus: "FAILED" }
+        });
+
+        console.log(error);
+
+        return NextResponse.json({
+            message: "Error processing event",
+            eventId: event.id
+        }, { status: 500 })
+    }
+
+    return NextResponse.json({
+        message: "Event processed successfully",
+        eventId: event.id
+    })
+}
